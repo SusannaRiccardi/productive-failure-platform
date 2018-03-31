174f789b51de3307e8b6f4aff3a85f08d95b9395
sri <susanna.riccardi@usi.ch>
2018-03-31 16:22:24 +0200

    Added routes in React

---
+++ productive-failure-platform/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+// Main page: login or home
+export default class Home extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+    render() {
+        return (
+            <div>
+                <h2>Home</h2>
+            </div>
+        );
+    }
+}
---
+++ productive-failure-platform/src/components/ProductiveFailureStageOneContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+export default class ProductiveFailureStageOneContainer extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+    render() {
+        return (
+            <div>
+                Stage1
+            </div>
+        );
+    }
+}
