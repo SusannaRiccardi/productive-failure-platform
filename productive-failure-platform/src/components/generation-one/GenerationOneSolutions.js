509e8cba5e4e3f043a278f1bc3fdb480d4682971
sri <susanna.riccardi@usi.ch>
2018-04-11 18:09:06 +0200

    Unified Representations and Elements in one component and added pencil and eraser for the canvas.

--- productive-failure-platform/src/components/generation-one/GenerationOneSolutions.js
+++ productive-failure-platform/src/components/generation-one/GenerationOneSolutions.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
-import GenerationOneElements from './GenerationOneElements';
-import GenerationOneRepresentations from './GenerationOneRepresentations';
+import { Row, Col, Button, Panel } from 'react-bootstrap';
 import config from '../../config';
+var _ = require('lodash');
+// literallycanvas variables
+var lc;
+var tools;
 
 
 export default class GenerationOneSolutions extends Component {
@@ -10,10 +12,28 @@ export default class GenerationOneSolutions extends Component {
         super(props)
         
         this.state = {
-            representationNumber : 0
+            representationNumber : 0,
+            selectedTool : 'pencil'
         }
 
         this.handleRepresentationNumberChange = this.handleRepresentationNumberChange.bind(this);
+        this.activateTool = this.activateTool.bind(this);
+    }
+
+    componentDidMount() {
+        lc = window.LC.init(document.getElementsByClassName('literally-core-container')[0]);
+        
+        tools = [
+            {
+                name : 'pencil',
+                el : document.getElementById('pencil'),
+                tool: new window.LC.tools.Pencil(lc)
+            }, {
+                name : 'eraser',
+                el : document.getElementById('eraser'),
+                tool : new window.LC.tools.Eraser(lc)
+            }
+        ]
     }
     
     // Function to handle the change of page of representation
@@ -25,21 +45,53 @@ export default class GenerationOneSolutions extends Component {
         })
     }
 
+    activateTool(tool) {
+        let toolObject = _.find(tools, (t) => {
+            return t.name === tool
+        });
+        console.log(toolObject)
+        lc.setTool(toolObject.tool);
+
+        this.setState({
+            selectedTool : tool
+        })
+    }
+
     render() {
+        var representation = config.representations[this.state.representationNumber];
         return (
             <div className="StageOneSolutions-container">
                 <Row>
                     <Col sm={6} md={3}>
-                        <GenerationOneElements 
-                            representationNumber={this.state.representationNumber}
-                            representation={config.representations[this.state.representationNumber]}
-                        />
+                        <Panel>
+                            <Panel.Heading>
+                                <Panel.Title componentClass="h3">Elements</Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body>
+                                {/* TODO: in config, put elements that are in the representation, not the one to be removed */}
+                                <div className="GenerateOneSolutions--container">
+                                    <div id="pencil" onClick={() => this.activateTool('pencil')} className={this.state.selectedTool.localeCompare('pencil') == 0 ? 'active' : ''}>
+                                        Pencil
+                                    </div>
+                                    <div id="eraser" onClick={() => this.activateTool('eraser')} className={this.state.selectedTool.localeCompare('eraser') == 0 ? 'active' : ''}>
+                                        Eraser
+                                    </div>
+                                </div>
+                            </Panel.Body>
+                        </Panel>
                     </Col>
                     <Col sm={6} md={9}>
-                        <GenerationOneRepresentations 
-                            representationNumber={this.state.representationNumber}
-                            representation={config.representations[this.state.representationNumber]}
-                        />
+                        <Panel>
+                            <Panel.Heading>
+                                <Panel.Title componentClass="h3">Representations</Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body>
+                                Constraint: {representation.constraint}
+
+                                <div className="literally-core-container">
+                                </div>
+                            </Panel.Body>
+                        </Panel>
                     </Col>
                 </Row>
 
