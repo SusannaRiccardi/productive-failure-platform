12246044bee0a165c588686ad6ae5c3ace2da3d4
sri <susanna.riccardi@usi.ch>
2018-04-11 11:26:00 +0200

    Added literally canvas (with an example)

--- productive-failure-platform/src/components/generation-one/GenerationOneRepresentations.js
+++ productive-failure-platform/src/components/generation-one/GenerationOneRepresentations.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+var lc;
 
 // Main page: login or home
 export default class StageOneRepresentations extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        lc = window.LC.init(document.getElementsByClassName('literally-core-container')[0]);
+    }
+
     render() {
         return (
             <Panel>
@@ -11,6 +20,9 @@ export default class StageOneRepresentations extends Component {
                 </Panel.Heading>
                 <Panel.Body>
                     Constraint: {this.props.representation.constraint}
+
+                    <div className="literally-core-container">
+                    </div>
                     
                 </Panel.Body>
             </Panel>
