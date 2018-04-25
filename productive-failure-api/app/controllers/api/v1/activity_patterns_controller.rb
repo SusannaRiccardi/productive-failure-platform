1ef1117c97700338af937077e4943828eab7af20
sri <susanna.riccardi@usi.ch>
2018-04-25 10:47:36 +0200

    Modified activity pattern so that the route returns a specific productive_failure

--- productive-failure-api/app/controllers/api/v1/activity_patterns_controller.rb
+++ productive-failure-api/app/controllers/api/v1/activity_patterns_controller.rb
@@ -1,22 +1,17 @@
 module Api::V1
     class ActivityPatternsController < ApplicationController
         def index
-            @activity_patterns = ActivityPattern.all
-            render json: @activity_patterns
+            @activity_pattern = ActivityPattern.find_by_productive_failure_id(params[:productive_failure_id])
+            render json: @activity_pattern
         end
 
         def create
             @activity_pattern = ActivityPattern.create(activity_pattern_params)
             render json: @activity_pattern
         end
-
-        def show
-            @activity_pattern = ActivityPattern.find(params[:id])
-            render json: @activity_pattern
-        end
           
         private
-          
+
         def activity_pattern_params
             params.require(:activity_pattern).permit(:productive_failure_id, :pattern_id)
         end
