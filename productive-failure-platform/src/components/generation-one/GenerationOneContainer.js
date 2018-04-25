23974243ee53b69d4472d045c0b0a3cda11cf251
sri <susanna.riccardi@usi.ch>
2018-04-25 11:39:04 +0200

    Fixed problem with the pattern changing every time the page was loaded.

--- productive-failure-platform/src/components/generation-one/GenerationOneContainer.js
+++ productive-failure-platform/src/components/generation-one/GenerationOneContainer.js
@@ -3,6 +3,7 @@ import PatternDescription from './PatternDescription';
 import GenerationOneSolutions from './GenerationOneSolutions';
 import axios from 'axios';
 import { Modal, Button } from 'react-bootstrap';
+var _ = require('lodash');
 
 
 export default class GenerationOneContainer extends Component {
@@ -11,15 +12,16 @@ export default class GenerationOneContainer extends Component {
 
         this.state = {
             patterns : [],
-            showModal : true
+            showModal : true,
+            activityPatterns : []
         }
         
-        this.requestPattern = this.requestPattern.bind(this);
+        this.checkActivityPattern = this.checkActivityPattern.bind(this);
         this.handleOpenCloseModal = this.handleOpenCloseModal.bind(this);
     }
 
     componentWillMount() {
-        this.requestPattern()
+        this.checkActivityPattern();
     }
 
     handleOpenCloseModal() {
@@ -28,54 +30,105 @@ export default class GenerationOneContainer extends Component {
         })
     }
 
-    requestPattern() {
-        // Request patterns that we'll use to be rendered in generation one
-        axios.get('http://localhost:3001/api/v1/patterns')
+    // TODO: please refactor this
+    checkActivityPattern() {
+        // TODO: look at better way to have id of productive failure activity
+        let url = window.location.href;
+        let splitString = _.split(url, '/');
+        let id = splitString[splitString.length - 2];
+
+        axios.get(`http://localhost:3001/api/v1/activity_patterns?productive_failure_id=${id}`)
         .then(response => {
-            this.setState({
-                patterns: response.data
-            })
+            if (response.data == null) {
+                axios.get('http://localhost:3001/api/v1/patterns')
+                .then(response => {
+                    this.setState({
+                        patterns: response.data
+                    })
+
+                    for (let i = 0; i < response.data.length; i++) {
+                        let activityPattern = {
+                            productive_failure_id: id,
+                            pattern_id: response.data[i].id
+                        }
+                        axios.post('http://localhost:3001/api/v1/activity_patterns', activityPattern)
+                        .then(response => {
+                            let activityPatterns = _.cloneDeep(this.state.activityPatterns)
+
+                            this.setState({
+                                activityPatterns: activityPatterns.push(response.data)
+                            })
+                        })
+                        .catch(error => console.log(error))
+                    }
+                })
+                .catch(error => console.log(error))
+            } else {
+                this.setState({
+                    activityPatterns : response.data
+                })
+
+                for (let i = 0; i < response.data.length; i++) {
+                    axios.get(`http://localhost:3001/api/v1/patterns/${response.data[i].pattern_id}`)
+                    .then(response => {
+                        let patterns = _.cloneDeep(this.state.patterns)
+                        patterns.push(response.data)
+
+                        this.setState({
+                            patterns: patterns
+                        })
+                    })
+                    .catch(error => console.log(error))
+                }
+            }
         })
         .catch(error => console.log(error))
     }
     
     render() {
-        return (
-            <div className="stage-1-container">
-                <Modal show={this.state.showModal} onHide={this.handleOpenCloseModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Productive Failure activity - generation, step one</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>How it works</h4>
-                        <p>In this box you see a pattern, composed of shapes. Your goal in this first step is to try to come up with 
-                        as many representations of the pattern as possible.</p>
-                        <p>You can click on the elements in the <b>Elements</b> section: this will allow you to use the selected tool 
-                        in the <b>Representations</b> section.</p>
-                        <p>You can add multiple descriptions for the pattern: just click on the <b>Forward</b> and <b>Back</b> buttons.</p>
-                        <p> For each representation, you will be provided with a constraint: this means that you won't be able to use certain 
-                        tools.</p>
-                        <p>You can modify the representations as many times as you want. Once you are ready to move on, click on
-                        the <b>Take me to the next stage</b> button.</p>
-
-                        <div className="modal-button">
-                            <Button bsStyle="primary" onClick={this.handleOpenCloseModal}>Gotcha</Button>
-                        </div>
-                    </Modal.Body>
-                </Modal>
-
-                <div className="stage-1--pattern">
-                    <PatternDescription 
+        if (this.state.patterns.length != 2) {
+            return (
+                // Loading page
+                <div></div>
+            )
+        } else {
+            return (
+                <div className="stage-1-container">
+                    <Modal show={this.state.showModal} onHide={this.handleOpenCloseModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Productive Failure activity - generation, step one</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <h4>How it works</h4>
+                            <p>In this box you see a pattern, composed of shapes. Your goal in this first step is to try to come up with 
+                            as many representations of the pattern as possible.</p>
+                            <p>You can click on the elements in the <b>Elements</b> section: this will allow you to use the selected tool 
+                            in the <b>Representations</b> section.</p>
+                            <p>You can add multiple descriptions for the pattern: just click on the <b>Forward</b> and <b>Back</b> buttons.</p>
+                            <p> For each representation, you will be provided with a constraint: this means that you won't be able to use certain 
+                            tools.</p>
+                            <p>You can modify the representations as many times as you want. Once you are ready to move on, click on
+                            the <b>Take me to the next stage</b> button.</p>
+    
+                            <div className="modal-button">
+                                <Button bsStyle="primary" onClick={this.handleOpenCloseModal}>Gotcha</Button>
+                            </div>
+                        </Modal.Body>
+                    </Modal>
+    
+                    <div className="stage-1--pattern">
+                        <PatternDescription 
+                            patterns={this.state.patterns}
+                        />
+                    </div>
+    
+                    <GenerationOneSolutions 
                         patterns={this.state.patterns}
                     />
+    
+                    {!this.state.showModal && <Button bsStyle="info" onClick={this.handleOpenCloseModal}>Get info about this step</Button>}
                 </div>
-
-                <GenerationOneSolutions 
-                    patterns={this.state.patterns}
-                />
-
-                {!this.state.showModal && <Button bsStyle="info" onClick={this.handleOpenCloseModal}>Get info about this step</Button>}
-            </div>
-        );
+            );
+        }
     }
 }
