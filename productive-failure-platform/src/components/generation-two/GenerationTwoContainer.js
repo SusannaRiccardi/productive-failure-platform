6d1c773ac583821cb6c29dffbff48ef9908230f3
sri <susanna.riccardi@usi.ch>
2018-04-28 11:39:44 +0200

    Added modal with information about step 2

--- productive-failure-platform/src/components/generation-two/GenerationTwoContainer.js
+++ productive-failure-platform/src/components/generation-two/GenerationTwoContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel, Row, Col } from 'react-bootstrap';
+import { Panel, Row, Col, Modal, Button } from 'react-bootstrap';
 import axios from 'axios';
 import PatternGeneration from './PatternGeneration';
 var lc;
@@ -10,8 +10,11 @@ export default class GenerationTwoContainer extends Component {
         super(props);
 
         this.state = {
-            representation : null
+            representation : null,
+            showModal : true
         }
+
+        this.handleOpenCloseModal = this.handleOpenCloseModal.bind(this);
     }
     
     componentWillMount() {
@@ -27,28 +30,37 @@ export default class GenerationTwoContainer extends Component {
         .catch(error => console.log(error))
     }
 
+    handleOpenCloseModal() {
+        this.setState({
+            showModal : !this.state.showModal
+        })
+    }
+
     render() {
         return (
             <div className="generation-two-container">
+                <Modal show={this.state.showModal} onHide={this.handleOpenCloseModal}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Productive Failure activity - generation, step two</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <h4>How it works</h4>
+                        In this stage, you will see a description of a pattern by someone else. Your goal here is to reproduce
+                        the original pattern from the given representation. Drag the elements below and complete the pattern.
+
+                        <div className="modal-button">
+                            <Button bsStyle="primary" onClick={this.handleOpenCloseModal}>Gotcha</Button>
+                        </div>
+                    </Modal.Body>
+                </Modal>
+
                 <Row>
-                    <Col sm={6} md={2}>
-                        <Panel>
-                            <Panel.Body>
-                                <div className="PatternDescription-description">
-                                    In this stage, you will see a description of a pattern by someone else. Your goal here is to reproduce
-                                    the original pattern from the given representation. Drag the elements below and complete the pattern.
-                                </div>
-                            </Panel.Body>
-                        </Panel>
-                    </Col>
-                    <Col sm={6} md={10}>
-                        <Panel>
-                            <Panel.Body>
-                                <div className="literally-core-container">
-                                </div>
-                            </Panel.Body>
-                        </Panel>
-                    </Col>
+                    <Panel>
+                        <Panel.Body>
+                            <div className="literally-core-container">
+                            </div>
+                        </Panel.Body>
+                    </Panel>
                 </Row>
 
                 <PatternGeneration />
