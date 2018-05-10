3d3225ab31fc3246175bbaa6e4956ad78c845c5a
sri <susanna.riccardi@usi.ch>
2018-05-10 16:23:22 +0200

    Modified tutorial of generation one

--- productive-failure-platform/src/components/generation-one/GenerationOneContainer.js
+++ productive-failure-platform/src/components/generation-one/GenerationOneContainer.js
@@ -3,6 +3,7 @@ import PatternDescription from './PatternDescription';
 import GenerationOneSolutions from './GenerationOneSolutions';
 import axios from 'axios';
 import { Modal, Button } from 'react-bootstrap';
+import GenerationOneTutorial from './GenerationOneTutorial';
 var _ = require('lodash');
 
 
@@ -102,27 +103,10 @@ export default class GenerationOneContainer extends Component {
         } else {
             return (
                 <div className="stage-1-container">
-                    <Modal show={this.state.showModal} onHide={this.handleOpenCloseModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Productive Failure activity - generation, step one</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <h4>How it works</h4>
-                            <p>In this box you see a pattern, composed of shapes. Your goal in this first step is to try to come up with 
-                            as many representations of the pattern as possible.</p>
-                            <p>You can click on the elements in the <b>Elements</b> section: this will allow you to use the selected tool 
-                            in the <b>Representations</b> section.</p>
-                            <p>You can add multiple descriptions for the pattern: just click on the <b>Forward</b> and <b>Back</b> buttons.</p>
-                            <p> For each representation, you will be provided with a constraint: this means that you won't be able to use certain 
-                            tools.</p>
-                            <p>You can modify the representations as many times as you want. Once you are ready to move on, click on
-                            the <b>Take me to the next stage</b> button.</p>
-    
-                            <div className="modal-button">
-                                <Button bsStyle="primary" onClick={this.handleOpenCloseModal}>Gotcha</Button>
-                            </div>
-                        </Modal.Body>
-                    </Modal>
+                    <GenerationOneTutorial 
+                        open={this.state.showModal}
+                        close={this.handleOpenCloseModal}
+                    />
     
                     <div className="stage-1--pattern">
                         <PatternDescription 
