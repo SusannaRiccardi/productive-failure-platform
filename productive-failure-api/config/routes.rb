52e3962fe5c4051a04c60ae87b14632fb785c850
sri <susanna.riccardi@usi.ch>
2018-05-22 09:34:03 +0200

    Fixed bug with route iterationConsolidation

--- productive-failure-api/config/routes.rb
+++ productive-failure-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     namespace :api do
         namespace :v1 do
-            resources :representations, :patterns, :activity_patterns, :representation, :reconstruct_patterns, :generated_patterns
+            resources :representations, :patterns, :activity_patterns, :representation, :reconstruct_patterns, :generated_patterns, :iteration_consolidations
         end
     end
 end
