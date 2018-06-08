b5e46d27c0ff86bee85790d4efb2c87978b55007
sri <susanna.riccardi@usi.ch>
2018-06-08 21:39:52 +0200

    Added new iteration consolidation

--- productive-failure-api/app/controllers/api/v1/iteration_consolidations_controller.rb
+++ productive-failure-api/app/controllers/api/v1/iteration_consolidations_controller.rb
@@ -4,5 +4,16 @@ module Api::V1
             @iteration_consolidation = IterationConsolidation.order("Random()").first
             render json: @iteration_consolidation
         end
+
+        def create
+            @iteration_consolidation = IterationConsolidation.create(iteration_consolidation)
+            render json: @iteration_consolidation
+          end
+          
+        private
+          
+        def iteration_consolidation
+            params.require(:iteration_consolidation).permit(:pattern, :rep1, :rep2, :rep3, :rep4, :rep5, :rep6)
+        end
     end
 end
