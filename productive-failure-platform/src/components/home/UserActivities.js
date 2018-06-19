8c6c3d62abb594c7dd330cfdedec33b963df30b7
sri <susanna.riccardi@usi.ch>
2018-06-19 14:48:20 +0200

    Modified style of productive failure activity button in home

--- productive-failure-platform/src/components/home/UserActivities.js
+++ productive-failure-platform/src/components/home/UserActivities.js
@@ -19,33 +19,50 @@ export default class UserActivities extends Component {
     }
 
     renderRepresentations() {
-        return this.state.productive_failures.map((rep, key) => {
-            let date = new Date(rep.created_at);
-            let day = date.getDate();
-            let month = date.getMonth() + 1;
-            if (month < 10) month = '0' + month
-            let year = date.getFullYear();
-            let hour = date.getHours();
-            if (hour < 10) hour = '0' + month
-            let minutes = date.getMinutes();
-            if (minutes < 10) minutes = '0' + minutes
-
-            let stringDate = day + '-' + month + '-' + year + ', ' + hour + ':' + minutes;
+        if (this.state.productive_failures.length == 0) {
             return (
-                <tr key={key}>
-                    <td>{rep.activity_type}</td>
-                    <td>{stringDate}</td>
+                <tr>
+                    <td colSpan="2">You have not started any activity yet.</td>
                 </tr>
             )
-        })
+        } else {
+            return this.state.productive_failures.map((rep, key) => {
+                let date = new Date(rep.created_at);
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                if (month < 10) month = '0' + month
+                let year = date.getFullYear();
+                let hour = date.getHours();
+                if (hour < 10) hour = '0' + month
+                let minutes = date.getMinutes();
+                if (minutes < 10) minutes = '0' + minutes
+    
+                let stringDate = day + '-' + month + '-' + year + ', ' + hour + ':' + minutes;
+                return (
+                    <tr key={key}>
+                        <td>{rep.activity_type}</td>
+                        <td>{stringDate}</td>
+                    </tr>
+                )
+            })
+        }
     }
     
     render() {
         return (
             <div className="UserActivities--container">
-                <Panel className="UserActivites--createPanel" onClick={() => this.props.createActivity('Iteration')}>
-                    <Panel.Body>Start new Productive Failure activity on Iteration</Panel.Body>
-                </Panel>
+                <Table responsive bordered>
+                    <thead>
+                        <tr>
+                            <th>Activities</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><Button bsStyle="link" onClick={() => this.props.createActivity('Iteration')}>Start new activity on Iteration</Button></td>
+                        </tr>
+                    </tbody>
+                </Table>
 
                 <Table responsive striped bordered>
                     <thead>
