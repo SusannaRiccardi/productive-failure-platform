737b58c040313e9ff2dda91c6dd8c595f98fa499
sri <susanna.riccardi@usi.ch>
2018-06-19 14:38:27 +0200

    Changed style in table of productive failures

--- productive-failure-platform/src/components/home/UserActivities.js
+++ productive-failure-platform/src/components/home/UserActivities.js
@@ -20,9 +20,21 @@ export default class UserActivities extends Component {
 
     renderRepresentations() {
         return this.state.productive_failures.map((rep, key) => {
+            let date = new Date(rep.created_at);
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            if (month < 10) month = '0' + month
+            let year = date.getFullYear();
+            let hour = date.getHours();
+            if (hour < 10) hour = '0' + month
+            let minutes = date.getMinutes();
+            if (minutes < 10) minutes = '0' + minutes
+
+            let stringDate = day + '-' + month + '-' + year + ', ' + hour + ':' + minutes;
             return (
                 <tr key={key}>
                     <td>{rep.activity_type}</td>
+                    <td>{stringDate}</td>
                 </tr>
             )
         })
@@ -31,14 +43,15 @@ export default class UserActivities extends Component {
     render() {
         return (
             <div className="UserActivities--container">
-                 <Panel className="UserActivites--createPanel" onClick={() => this.props.createActivity('iteration')}>
+                <Panel className="UserActivites--createPanel" onClick={() => this.props.createActivity('Iteration')}>
                     <Panel.Body>Start new Productive Failure activity on Iteration</Panel.Body>
                 </Panel>
 
-                <Table responsive striped>
+                <Table responsive striped bordered>
                     <thead>
                         <tr>
                             <th>Type</th>
+                            <th>Created</th>
                         </tr>
                     </thead>
                     <tbody>
