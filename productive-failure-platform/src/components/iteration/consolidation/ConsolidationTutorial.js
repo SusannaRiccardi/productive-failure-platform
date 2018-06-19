78e0bb9bfd54acabebf0c7b4e43c5997b31b40e5
sri <susanna.riccardi@usi.ch>
2018-06-19 19:58:28 +0200

    Modified tutorial for consolidation

--- productive-failure-platform/src/components/iteration/consolidation/ConsolidationTutorial.js
+++ productive-failure-platform/src/components/iteration/consolidation/ConsolidationTutorial.js
@@ -13,8 +13,12 @@ export default class ConsolidationTutorial extends Component {
                 <Modal.Body>
                     <div className="tutorial-container">
                         <h4>How it works</h4>
-                        <p className="tutorial-text">In this final step, you will see a pattern and <b>six representations</b> that are related to the pattern.</p>
+                        <p className="tutorial-text">In this final step, you will see a pattern and <b>six representations</b> that are related to the pattern that 
+                        you can see at the top of the page.</p>
                         <p className="tutorial-text">Your <b>goal</b> here is to rank the representations, from best to worst.</p>
+
+                        <p className="tutorial-text">In order to do so, you can drag one representation from the list in the right column and you
+                        can drop it in the boxes with the rankings in the left column.</p>
                     </div>
                     <div className="modal-button">
                         <Button bsStyle="primary" onClick={this.props.close}>Got it</Button>
